cf615ed846e94b82438a6db898496e4332c415b9
ceyhxun <ceyhunqasimov028@gmail.com>
2025-05-04 23:01:50 +0400

    Add files via upload

---
+++ reference.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    var content = document.getElementById("referenceContent");
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}

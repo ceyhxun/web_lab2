242c65d6ab028d7da934ff06d973d408202c6002
ceyhxun <ceyhunqasimov028@gmail.com>
2025-05-07 00:56:04 +0400

    Delete reference.js

--- reference.js
+++
@@ -1,8 +0,0 @@
-function toggleDropdown() {
-    var content = document.getElementById("referenceContent");
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
-}
